897eb476ee5f4a30e42f7702af44ffc8640b42ca
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-06-15 18:31:36 +0400

    Initial Commit

---
+++ lesson 3.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson 3.01
+//
+//  Created by Aleksandr Mamlygo on 15.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
